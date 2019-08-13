a921864fe91c14d9a6146f37ccb9279e4d980052
Hubert Piesniak <hubert_piesniak@epam.com>
2019-08-13 15:39:39 +0200

    create simple gallery

--- index.js
+++ index.js
@@ -7,7 +7,69 @@
 //   'create' - new image page
 
 window.addEventListener('DOMContentLoaded', (event) => {
+  applyPathSettings()
   console.log('path: ', window.location.pathname); // can be removed
 
   // TODO: your code is here
 });
+
+function applyPathSettings() {
+  var path = window.location.pathname;
+  switch (path) {
+    case '/':
+      displayGallery();
+      break;
+    case '/create':
+      setCreateSettings();
+      break;
+    case '/images':
+      setImageSettings();
+      break;
+  }
+}
+
+function displayGallery() {
+  document.title = "Gallery | Academy";
+  document.getElementById("gallery").style.display = 'inline';
+  fetchImages();
+}
+
+function fetchImages() {
+  fetch('http://localhost:3000/images')
+    .then(resp => resp.json())
+    .then(displayJson)
+}
+
+function displayJson(json){
+  const gallery = document.querySelector('#gallery');
+  for(var obj of json){
+    let galleryBox = document.createElement('div');
+    galleryBox.className = "gallery-box";
+    gallery.appendChild(galleryBox);
+
+    let txt = document.createElement('p');
+    txt.textContent = obj.title;
+    galleryBox.appendChild(txt);
+
+    let button = document.createElement('button');
+    button.className = "delete-button";
+    button.textContent = "delete";
+    galleryBox.appendChild(button);
+
+    let img = document.createElement('img');
+    img.src = obj.src;
+    galleryBox.appendChild(img);
+
+    console.log(obj.title)
+  }
+}
+
+function setCreateSettings() {
+  document.title = "Create | Academy"
+  document.getElementById("create").style.display = 'inline';
+}
+
+function setImageSettings() {
+  document.title = "Image | Academy"
+  document.getElementById("image").style.display = 'inline';
+}
