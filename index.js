a9f5683632ec3ef67c4772b40ca96890fa963ce0
Hubert Piesniak <hubert_piesniak@epam.com>
2019-08-13 13:52:36 +0200

    add path specific settings

--- index.js
+++ index.js
@@ -7,7 +7,38 @@
 //   'create' - new image page
 
 window.addEventListener('DOMContentLoaded', (event) => {
+  applyPathSettings()
   console.log('path: ', window.location.pathname); // can be removed
 
   // TODO: your code is here
 });
+
+function applyPathSettings() {
+  var path = window.location.pathname;
+  switch (path) {
+    case '/':
+      setGallerySettings();
+      break;
+    case '/create':
+      setCreateSettings();
+      break;
+    case '/images':
+      setImageSettings();
+      break;
+  }
+}
+
+function setGallerySettings() {
+  document.title = "Gallery | Academy"
+  document.getElementById("gallery").style.display = 'inline';
+}
+
+function setCreateSettings() {
+  document.title = "Create | Academy"
+  document.getElementById("create").style.display = 'inline';
+}
+
+function setImageSettings() {
+  document.title = "Image | Academy"
+  document.getElementById("image").style.display = 'inline';
+}
